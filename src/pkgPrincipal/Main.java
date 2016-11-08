2a38e28e8bb965231368b0b51fd8c08f3a8ebea9
UyuCilDel <UyuCilDel>
2016-11-08 10:35:54 +0000

    yup

--- src/pkgPrincipal/Main.java
+++ src/pkgPrincipal/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         clControladorEventos controladorEventos = new clControladorEventos();
         controladorEventos.iniciarFramePrincipal();
+        System.out.println("funciona?");
     }
 
 }
